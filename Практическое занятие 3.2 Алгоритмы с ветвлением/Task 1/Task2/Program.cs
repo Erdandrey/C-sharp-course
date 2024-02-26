327597d98ce47063531c2614909fb30e427d22b2
Andrew Ryabous <erdandrey@yandex.ru>
2024-02-27 01:37:24 +0300

    Задача 2

--- Практическое занятие 3.2 Алгоритмы с ветвлением/Task 1/Task2/Program.cs
+++ Практическое занятие 3.2 Алгоритмы с ветвлением/Task 1/Task2/Program.cs
@@ -10,6 +10,32 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите координату X первой точки");
+            int X1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите координату Y первой точки");
+            int Y1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите координату X второй точки");
+            int X2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите координату Y второй точки");
+            int Y2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите координату X третьей точки");
+            int X3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите координату Y третьей точки");
+            int Y3 = Convert.ToInt16(Console.ReadLine());
+            double AB = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            double AC = Math.Sqrt(Math.Pow(X3 - X1, 2) + Math.Pow(Y3 - Y1, 2));
+            if (AB < AC)
+            {
+                Console.WriteLine("Точка B расположена ближе к точке А. Расстояние между точками: {0}", AB);
+            }
+            else if (AB > AC)
+            {
+                Console.WriteLine("Точка C расположена ближе к точке А. Расстояние между точками: {0}", AC);
+            }
+            else 
+            { 
+                Console.WriteLine("Точки C и B расположены на одинаковом расстоянии от точки А. Это расстояние равно: {0}", AC);
+            }
         }
     }
 }
