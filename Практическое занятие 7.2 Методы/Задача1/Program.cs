2db4db3b53aa009489fc55b38bd7072b208c3914
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-01 23:05:48 +0300

    Задачи по теме Методы

---
+++ Практическое занятие 7.2 Методы/Задача1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static double GetSquare(int x, int y, int z)
+        {
+            double PP = ((double)x + y + z) / 2;
+            return Math.Sqrt(PP * (PP - x) * (PP - y) * (PP - z));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны первого треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стороны второго треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            double S1 = GetSquare(a1, b1, c1);
+            double S2 = GetSquare(a2, b2, c2);
+            if (S1 > S2) 
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else if ((S1 < S2))
+            {
+                Console.WriteLine("Площадь второго треугольника больше");
+            }
+            else
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+        }
+    }
+}
---
+++ Практическое занятие 7.2 Методы/Задача2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача2
+{
+    internal class Program
+    {
+        static void GetCubeParams(int D, out int V,out int S)
+        {
+            V = D*D*D;
+            S = 6*D*D;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int D = Convert.ToInt32(Console.ReadLine());
+            int Square, Volume;
+            GetCubeParams(D,out Volume,out Square);
+            Console.WriteLine("Объём куба будет равен {0}, а площадь поверхности {1}", Volume, Square);
+        }
+    }
+}
