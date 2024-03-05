a8c648a9648e134627cf7e2fce9090875ad4e0af
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-06 02:50:08 +0300

    Классы и объекты

---
+++ Практическое занятие 10.2 Классы и объекты/Задача1/Angle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool flag = true;
+
+        public int Gradus 
+        {
+            get { return gradus; }
+            set 
+            {
+                if (value < 0)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    gradus = value;
+                }
+            }
+        }
+        public int Min
+        {
+            get { return min; }
+            set 
+            {
+                if (value < 0)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Gradus = Gradus + value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+        public int Sec
+        {
+            get { return sec; }
+            set 
+            {
+                if (value < 0)
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Min = Min + value / 60;
+                    sec = value % 60;
+                }               
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public double ToRadians() 
+        {
+            double rad = gradus*Math.PI/180 + min * Math.PI / 10800 + sec * Math.PI / 648000;
+            return rad;
+        }
+    }
+}
---
+++ Практическое занятие 10.2 Классы и объекты/Задача1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Angle An;
+                do
+                {
+                    Console.WriteLine("Введите число градусов угла");
+                    int g = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите число минут угла");
+                    int m = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите число секунд угла");
+                    int s = Convert.ToInt32(Console.ReadLine());
+                    An = new Angle(g, m, s);
+                } while (An.flag == false);
+                double r = An.ToRadians();
+                Console.WriteLine("{0}  {1}   {2}", An.Gradus, An.Min, An.Sec); // проверка корректности перевода "излишних" минут и секунд
+                Console.WriteLine("Значение угла в радианах: {0}", r);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+    }
+}
