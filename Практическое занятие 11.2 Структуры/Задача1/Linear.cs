12201a0b86cc993ab5b66e65ebd768ef0ddc1eaa
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-09 22:39:00 +0300

    Задачи по структурам

---
+++ Практическое занятие 11.2 Структуры/Задача1/Linear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            return $"Корень уравнения: {-b/k}";
+        }
+    }
+}
---
+++ Практическое занятие 11.2 Структуры/Задача1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициенты линейного уравнения k и b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Linear lin = new Linear(k,b);
+            Console.WriteLine(lin.Root());
+        }
+    }
+}
