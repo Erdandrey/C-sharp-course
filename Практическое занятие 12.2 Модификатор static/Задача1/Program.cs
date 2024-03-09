62afce5abf725686eb7afdf869166cdbd4a2a4c2
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-09 23:30:48 +0300

    Модификатор Static

---
+++ Практическое занятие 12.2 Модификатор static/Задача1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты центра окружности");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты точки");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Длина окружности {Circle.GetLength(r)}. Площадь круга {Circle.GetSquare(r)}.");
+            Circle.BelongsToCircle(r, x, y, x0, y0 );
+            Console.ReadKey();
+        }
+    }
+}
