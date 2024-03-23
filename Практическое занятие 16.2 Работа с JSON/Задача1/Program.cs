b5435a76116917876ae368f0ac4cb35b878aec8e
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-23 21:24:24 +0300

    Работа с JSON

---
+++ Практическое занятие 16.2 Работа с JSON/Задача1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Goods[] goods = new Goods[n];
+
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine("Введите идентификатор товара");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите название товара");
+                string name = Console.ReadLine();
+                Console.WriteLine("Введите цену товара");
+                double price = Convert.ToDouble(Console.ReadLine());
+                goods[i] = new Goods() { Id = id, Name = name, Price = price };
+            }
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string JsonString = JsonSerializer.Serialize(goods, options);
+
+            StreamWriter sw = new StreamWriter("../../../Goods.json");
+            sw.WriteLine(JsonString);
+            sw.Close();
+        }
+    }
+}
---
+++ Практическое занятие 16.2 Работа с JSON/Задача2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Задача2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string JsonString = String.Empty;
+            StreamReader sr = new StreamReader("../../../Goods.json");
+            JsonString = sr.ReadToEnd();
+            sr.Close();
+            Goods[] goods = JsonSerializer.Deserialize < Goods[]>(JsonString);
+            Goods MaxPrice = goods[0];
+            foreach (Goods good in goods) 
+            { 
+                if (good.Price > MaxPrice.Price)
+                {
+                    MaxPrice = good;
+                }
+            }
+            Console.WriteLine($"{MaxPrice.Id} {MaxPrice.Name} {MaxPrice.Price}");
+            Console.ReadKey();
+        }
+    }
+}
