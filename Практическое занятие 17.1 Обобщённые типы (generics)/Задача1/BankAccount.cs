643c3f08e2df51d261f2eaf728fa4c6d8d611d59
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-24 15:48:07 +0300

    Обобщённые типы

---
+++ Практическое занятие 17.1 Обобщённые типы (generics)/Задача1/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class BankAccount <T>
+    {
+        private T accountType { get; set; }
+        private string ownerName { get; set; }
+        private double accountBalance { get; set; }
+
+        public void SetInfo()
+        {
+            Console.WriteLine("Введите тип счёта");
+            accountType = (T)(Convert.ChangeType(Console.ReadLine(),typeof(T)));
+            Console.WriteLine("Введите ФИО владельца");
+            ownerName = Console.ReadLine();
+            Console.WriteLine("Введите баланс счёта");
+            accountBalance = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public string GetInfo()
+        {
+            return $"Тип счёта: {accountType}\nВладелец счёта: {ownerName}\nБаланс счёта: {accountBalance}";
+        }
+    }
+}
---
+++ Практическое занятие 17.1 Обобщённые типы (generics)/Задача1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> acc1 = new BankAccount<int>();
+            acc1.SetInfo();
+            Console.WriteLine(acc1.GetInfo());
+
+            BankAccount<string> acc2 = new BankAccount<string>();
+            acc2.SetInfo();
+            Console.WriteLine(acc2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
