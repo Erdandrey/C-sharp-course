5e773f894e2f7297d58ccdf003b91dd8c482ff48
Andrew Ryabous <erdandrey@yandex.ru>
2024-03-25 03:27:25 +0300

    Асинхронное программирование

---
+++ Практическое занятие 23.2 Асинхронное программирование/Задача1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Задача1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            GetFactorialAsync(N);
+            Console.ReadKey();
+        }
+
+        static void GetFactorial(int n) 
+        {
+            int S = 1;
+            for (int i = 1; i < n+1; i++) 
+            {
+                S *= i;
+                Thread.Sleep(20);
+            }
+            Console.WriteLine(S);
+        }
+        static async void GetFactorialAsync( int n)
+        {
+            await Task.Run(() => { GetFactorial(n); });
+        }
+    }
+}
